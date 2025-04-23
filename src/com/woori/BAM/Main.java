0acd027a4e6966deb91029bd0db03a08f65a3034
KIMSOSLE <pdw05027@gmail.com>
2025-04-23 13:07:32 +0900

    article write 구현중

--- src/com/woori/BAM/Main.java
+++ src/com/woori/BAM/Main.java
@@ -9,14 +9,17 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		
 		int id = 1;
-
+		// 저장을 위한 변수이기 때문에 반복문 밖에 선언
+		// 반복문 내 선언 시 매번 초기화됨
 		while (true) {
 			System.out.printf("명령어) ");
-			String cmd = sc.nextLine().trim();
+			String cmd = sc.nextLine().trim(); // 공백제거를 위해 trim() 함수 사용
 
 			if (cmd.length() == 0) {
+				// 문자열의 길이가 0인지 확인 즉, cmd가 빈 문자열("")인지 검사
+				// 사용자가 아무 명령어를 입력하지 않았다면
 				System.out.println("명령어를 입력해 주세요.");
-				continue;
+				continue; // 아래쪽에 있는 출력이 실행되지 않도록 기다리기 위해 사용
 			}
 			
 			if (cmd.equals("article list")) {
