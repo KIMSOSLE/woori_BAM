bfa8f776bb125dc851186bcbac208213d135019e
KIMSOSLE <pdw05027@gmail.com>
2025-04-24 15:01:46 +0900

    article list _ size(), get() 학습

--- src/com/woori/BAM/Main.java
+++ src/com/woori/BAM/Main.java
@@ -12,7 +12,6 @@ public class Main {
 
 		int id = 1;
 		List<Article> articles = new ArrayList<>();
-		// List 인터페이스 - ArryList 구현 클레스
 
 		while (true) {
 			System.out.printf("명령어) ");
@@ -22,36 +21,31 @@ public class Main {
 				System.out.println("명령어를 입력해 주세요.");
 				continue;
 			}
-
+			// List 메서드 중 size() 이용 (Data 유무를 개체의 갯수(크기)로 변환)
 			if (cmd.equals("article list")) {
-				System.out.println("게시글이 없습니다.");
+				if (articles.size() == 0) {
+					System.out.println("존재하는 게시글이 없습니다.");
+					continue;
+				}
+				// 배열을 사용해 get() 이용 (객체를 리턴 받음)
+				System.out.printf("번호    |    제목\n");
+				for (int i = articles.size() -1; i >= 0; i--) {
+					// article은 articles.get()으로 받은 객체를 재사용하기 위해 저장 용도로 사용
+					Article article = articles.get(i);
+					System.out.printf("%d       |     %s\n", article.id, article.title);
+				}
 			} else if (cmd.equals("article write")) {
 				System.out.print("제목 : ");
 				String title = sc.nextLine().trim();
 				System.out.print("내용 : ");
 				String body = sc.nextLine().trim();
-				
-//				Article article = new Article(); // 최적화 X, // article은 지역변수 즉, 저장되지 않음
-				Article article = new Article(id, title, body); // 최적화 작업 : 생성자를 이용해 초기화, 인자를 통해 생성자 호출
-				
-//				article.id = id;
-//				article.title = title;
-//				article.body = body;
 
+				Article article = new Article(id, title, body); // 최적화 작업 : 생성자를 이용해 초기화, 인자를 통해 생성자 호출
 				// 진짜 저장은 아래의 문장을 통해서 진행
 				articles.add(article); // List 구조인 ArrayList 객체 articles에 저장
-				
-				// 최적화2
-//				articles.add(new Article(id, title, body));
 
 				System.out.println(id + "번 글이 생성되었습니다.");
 				id++;
-			} else if (cmd.equals("test")) { // 명령어 test로 articles 저장된 내용 확인
-				for (int i = 0; i < articles.size(); i++) {
-					System.out.println(articles.get(i).id);
-					System.out.println(articles.get(i).title);
-					System.out.println(articles.get(i).body);
-				}
 			} else {
 				System.out.println("존재하지 않는 명령어 입니다.");
 			}
